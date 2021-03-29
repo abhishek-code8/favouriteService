8ada8d809c1923ba5663f5bcb8ef6ce38b07d69b
Abhishek Sharma <abhishek.sharma8@ibm.com>
2021-03-29 17:56:41 +0530

    JWT

--- src/main/java/com/stackroute/favouritegif/FavouriteServiceApplication.java
+++ src/main/java/com/stackroute/favouritegif/FavouriteServiceApplication.java
@@ -1,10 +1,26 @@
 package com.stackroute.favouritegif;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.stackroute.favouritegif.jwtfilter.JwtFilter;
 
 @SpringBootApplication
 public class FavouriteServiceApplication {
+	
+	  @Bean
+	    public FilterRegistrationBean jwtFilter() {
+		  FilterRegistrationBean<Filter> filter = new FilterRegistrationBean <>();
+		  filter.addUrlPatterns("/api/v1/gifs/*");
+		  filter.setFilter(new JwtFilter());
+		  return filter;
+
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FavouriteServiceApplication.class, args);
